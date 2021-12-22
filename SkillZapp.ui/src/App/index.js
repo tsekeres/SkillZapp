cb2989fef72859071447b954660d5195cf47bcc7
tsekeres <twsekeres@gmail.com>
2021-12-21 22:36:35 -0600

    Auth FE/BE set up

--- SkillZapp.ui/src/App/index.js
+++ SkillZapp.ui/src/App/index.js
@@ -7,17 +7,18 @@ import NavBar from '../components/NavBar/NavBar';
 
 function App() {
   const [user, setUser] = useState(null);
+  // const [components, setComponents] = useState([]);
 
   useEffect(() => {
-    firebase.auth().onAuthStateChanged((userObj) => {
-      if (userObj) {
-        userObj.getIdToken().then((token) => sessionStorage.setItem('token', token));
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        user.getIdToken().then((token) => sessionStorage.setItem('token', token));
         const userInfoObj = {
-          firstName: userObj.firstName,
-          lastName: userObj.lastName,
-          profileImage: userObj.photoURL,
-          uid: userObj.uid,
-          user: userObj.email.split('@')[0],
+          firstName: user.firstName,
+          lastName: user.lastName,
+          profileImage: user.photoURL,
+          uid: user.uid,
+          user: user.email.split('@')[0],
         };
         setUser(userInfoObj);
       } else setUser(false);
--- SkillZapp.ui/src/helpers/auth.js
+++ SkillZapp.ui/src/helpers/auth.js
@@ -1,8 +1,35 @@
 import firebase from 'firebase/app';
+import axios from 'axios';
+
+axios.interceptors.request.use(
+  (request) => {
+    const token = sessionStorage.getItem('token');
+
+    if (token != null) {
+      request.headers.Authorization = `Bearer ${token}`;
+    }
+    return request;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 const signInUser = () => {
   const provider = new firebase.auth.GoogleAuthProvider();
-  firebase.auth().signInWithPopup(provider);
+  firebase.auth().signInWithPopup(provider).then((user) => {
+      if (user.additionalUserInfo?.isNewUser) {
+          const userInfoObj = {
+            firstName: user.user?.firstName,
+            lastName: user.user?.lastName,
+            profileImage: user.user?.photoURL,
+            uid: user.user?.uid,
+            user: user.user?.email.split('@')[0],
+          };
+          addUser(userInfoObj);
+          window.location.href = '/';
+        }
+    });
 };
 
 const signOutUser = () => new Promise((resolve, reject) => {
